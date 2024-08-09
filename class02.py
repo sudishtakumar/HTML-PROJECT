d796067d49d7d162933569f42f4c3ba2ad050cff
sudishta kumar Yadav <142447303+sudishtakumar@users.noreply.github.com>
2024-08-09 11:11:59 +0530

    Add files via upload

---
+++ class02.py
@@ -0,0 +1,29 @@
+a= "1"
+# a= 1
+b= "2"
+#b= 2
+print( int (a)+ int(b))
+
+# Implicit Type Casting
+
+c=1.9 # pyton automatically converts b to float
+d= 8  # python automatically convdts, a to int 
+print( c+d)# python automcally  converts c to  float as it is a flaot additon 
+
+
+
+
+
+ # user form input function 
+ # Syntex:::  varible= input()
+ 
+ 
+a = input(" Enter your name::")
+print( " My name is ", a)
+
+X= input( " Enter first number:")
+Y = input ( " Enter second number :")
+print( X+Y)
+print( int(X)+ int( Y))
+
+ 
